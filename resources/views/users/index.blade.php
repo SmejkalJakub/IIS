0950355ea7d3383d53757f27aba290ecaaa4f4a3
DNejedly <57711057+DNejedly@users.noreply.github.com>
2020-11-08 21:02:34 +0100

    sort html into folders

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -16,7 +16,7 @@
     </div>
     <ul class="nav navbar-nav">
       <li><a href="{{route('home')}}">Home</a></li>
-      <li><a href="tests">Tests</a></li>
+      <li><a href="../tests">Tests</a></li>
         @if(Auth::user()->hasRole('admin'))
             <li class="active"><a href="{{route('user-list')}}">Users</a></li>
         @endif
--- resources/views/users/user_config.blade.php
+++ resources/views/users/user_config.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>Edit User</title>
+  <title>Settings</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
@@ -17,7 +17,7 @@
         </div>
         <ul class="nav navbar-nav">
             <li><a href="{{route('home')}}">Home</a></li>
-            <li><a href="tests">Tests</a></li>
+            <li><a href="../tests">Tests</a></li>
             @if(Auth::user()->hasRole('admin'))
                 <li class="active"><a href="{{route('user-list')}}">Users</a></li>
             @endif
@@ -34,40 +34,41 @@
 
 <div class="container">
     <h2>Settings</h2>
-    <form action="{{url('save-edit')}}" method="POST" id="regForm">
+    <form action="{{url('save-config')}}" method="POST" id="regForm">
       {{ csrf_field() }}
       <div class="form-group">
-        <input type="hidden" id="id" name="id" value={{$user->id}}>
-      </div>
-      <div class="form-group">
           <label for="inputFirstName">First Name</label>
-          <input type="text" class="form-control" id="inputFirstName" placeholder="Enter full name" name="first_name" value={{$user->first_name}}>
+          <input type="text" class="form-control" id="inputFirstName" placeholder="Enter full name" name="first_name" value={{Auth::user()->first_name}}>
           @if ($errors->has('first_name'))
               <span class="error">{{ $errors->first('first_name') }}</span>
           @endif
       </div>
       <div class="form-group">
         <label for="inputSurname">Surname</label>
-        <input type="text" class="form-control" id="inputSurname" placeholder="Enter full name" name="surname" value={{$user->surname}}>
+        <input type="text" class="form-control" id="inputSurname" placeholder="Enter full name" name="surname" value={{Auth::user()->surname}}>
         @if ($errors->has('surname'))
             <span class="error">{{ $errors->first('surname') }}</span>
         @endif
     </div>
       <div class="form-group">
           <label for="inputEmailAddress">Email:</label>
-          <input type="email" class="form-control" id="inputEmailAddress" placeholder="Enter email" name="email" value={{$user->email}}>
+          <input type="email" class="form-control" id="inputEmailAddress" placeholder="Enter email" name="email" value={{ Auth::user()->email}}>
           @if ($errors->has('email'))
               <span class="error">{{ $errors->first('email') }}</span>
           @endif
       </div>
       <div class="form-group">
-        <label for="inputRole">Role:</label>
-        <select id="inputRole" name="role">
-            <option value="student">Student</option>
-            <option value="assistant">Assistant</option>
-            <option value="profesor">Profesor</option>
-            <option value="admin">Admin</option>
-          </select>
+        <label for="inputNewPassword">New Password:</label>
+          <label for="newPassword"></label><input type="password" class="form-control" id="newPassword" placeholder="New password" name="newPassword">
+        <b>
+            @if($errors->has('notMatching'))
+                {{ implode('', $errors->all(':message')) }}
+            @endif
+        </b>
+    </div>
+    <div class="form-group">
+        <label for="inputNewPasswordConfirm">New Password Again:</label>
+        <label for="newPasswordConfirm"></label><input type="password" class="form-control" id="newPasswordConfirm" placeholder="New password again" name="newPasswordConfirm">
     </div>
       <button type="submit" class="btn btn-default">Save</button>
     </form>
