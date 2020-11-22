197a2597f56e457f519c36bb63301e1d14abfe29
Jakub Smejkal <smejkal.jakub99@gmail.com>
2020-11-22 20:13:44 +0100

    Home fixed

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,6 +6,28 @@
 @include('layouts.header')
 @include('layouts.navbar', ['activeBar' => 'home'])
 
+Name: {{Auth::User()->first_name}} {{Auth::User()->surname}} <br>
+Email: {{Auth::User()->email}} <br>
+Role: {{Auth::User()->role}} <br>
+
+
+
+<a role="button" href="{{route('tests..', ['student', 'available'])}}" class="btn btn-lg btn-success ">Available tests</a><br><br>
+<a role="button" href="{{route('tests..', ['student', 'history'])}}" class="btn btn-lg btn-success ">My filled tests</a><br><br>
+
+
+@if(App\Http\Controllers\AuthController::checkUser('assistant'))
+    <a role="button" href="{{route('tests..', ['assistant', 'available'])}}" class="btn btn-lg btn-success ">Available corrections</a><br><br>
+    <a role="button" href="{{route('tests..', ['assistant', 'history'])}}" class="btn btn-lg btn-success ">My corrections</a><br><br>
+@endif
+@if(App\Http\Controllers\AuthController::checkUser('profesor'))
+    <a role="button" href="{{route('tests..', ['professor', 'history'])}}" class="btn btn-lg btn-success ">My tests</a><br><br>
+@endif
+
+@if(App\Http\Controllers\AuthController::checkUser('admin'))
+    <a role="button" href="{{route('user-list')}}" class="btn btn-lg btn-success ">Manage users</a><br><br>
+@endif
+
 </body>
 </html>
 
